67b1c8bfe1fab5c45088fb34865e652b59413490
Kai Aichholz <50125442+kaiba42@users.noreply.github.com>
2025-11-22 00:13:08 -0800

    fix: missing quotes on sha256 hash in db-pilot.rb

--- Casks/db-pilot.rb
+++ Casks/db-pilot.rb
@@ -1,7 +1,7 @@
 cask "db-pilot" do
   version "1.4.16"
   # Calculated with shasum -a 256
-  sha256 f7998384d45b4a7bcedc5e463f176970a539967588b7277a57981e445efdff66
+  sha256 "f7998384d45b4a7bcedc5e463f176970a539967588b7277a57981e445efdff66"
 
   url "https://releases.dbpilot.io/DB%20Pilot_#{version}_aarch64.dmg"
   name "DB Pilot"

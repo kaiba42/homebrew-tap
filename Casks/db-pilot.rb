254f848a41c4707bea1ac708e0c2f285acc4af29
Kai Aichholz <50125442+kaiba42@users.noreply.github.com>
2025-11-22 01:07:28 -0800

    fix: quoting issues

--- Casks/db-pilot.rb
+++ Casks/db-pilot.rb
@@ -1,15 +1,13 @@
 cask "db-pilot" do
   version "1.4.16"
-  # Calculated with shasum -a 256
   sha256 "f7998384d45b4a7bcedc5e463f176970a539967588b7277a57981e445efdff66"
-
+  
   url "https://releases.dbpilot.io/DB%20Pilot_#{version}_aarch64.dmg"
   name "DB Pilot"
   desc "Database management tool"
-  homepage "https://dbpilot.io"
-
-  app "DB Pilot.app"
-
-  # If you want to specify architecture
+  homepage "https://dbpilot.io/"
+  
   depends_on arch: :arm64
+  
+  app "DB Pilot.app"
 end
